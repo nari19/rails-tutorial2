401744920713eb91a8384ed0e90452d078268f2c
nari19 <kazuki7793996@gmail.com>
2019-02-24 18:26:36 +0900

    static-pages test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_response :success
   end
 
 end
