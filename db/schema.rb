6a38c9245aa07815a06ad1c3755e291b4f4b9dbb
nari19 <kazuki7793996@gmail.com>
2019-04-11 18:34:12 +0900

    セキュアなパスワードの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_003132) do
+ActiveRecord::Schema.define(version: 2019_04_07_063111) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
